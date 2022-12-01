0d37c684ef5b42a1a4fc4272ac9b153087c48da9
Anisha <anishalathwalal1983@gmail.com>
2022-12-01 19:53:52 +0530

    To ask a input from the user and to print the sum of all natural numbers upto the input..

---
+++ act8.py
@@ -0,0 +1,11 @@
+# Sum of n natural numbers
+# ask a user for natural numner
+# print total from 1 to n
+
+a= input("Enter a number here: ")
+total= 0
+i= 1
+while i<=int(a):
+    total = total+4
+    i= i+1
+print(total)

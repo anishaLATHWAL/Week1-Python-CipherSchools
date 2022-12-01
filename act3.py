748792793a2602d91e08ae8075a2cef953f5d023
Anisha <anishalathwalal1983@gmail.com>
2022-12-01 19:39:23 +0530

    To calculate the average of the given input user..

---
+++ act3.py
@@ -0,0 +1,5 @@
+#  To calculate the average of the given input User
+a = input("Enter the first number: ")
+b = input("Enter the first Second: ")
+c = input("Enter the first Third: ")
+print("Average of three numbers:", {(int(a) + int(b) + int(c)) / 3})

e85d7ae8b633bf6d5c2f716999f42f1cdc6efa6f
Anisha <anishalathwalal1983@gmail.com>
2022-12-01 19:51:43 +0530

    To input name and age for giving permission to watch coco in python..

---
+++ act7.py
@@ -0,0 +1,6 @@
+name= input("Enter you name: ")
+age= int(input("Enter you age: "))
+if age >=10 and name[0]=="A"or name[0]=="a":
+    print("You can watch coco")
+else:
+    print("sorry! you cannot watch coco")
